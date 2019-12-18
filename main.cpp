e32bfc1bdb0dd99f6840d60a95f266c7e1ca39de
strvworks <strv13570@gmail.com>
2019-12-19 04:31:49 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -34,11 +34,8 @@ int main()
         return -1;
     }    
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
